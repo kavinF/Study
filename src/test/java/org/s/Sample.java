56712d42aaad989372c51bb2c12214e370e0b4c1
kavinF <kavinf77@gmail.com>
2022-10-07 14:31:05 +0530

    1st push

---
+++ src/test/java/org/s/Sample.java
@@ -0,0 +1,19 @@
+package org.s;
+
+public class Sample {
+
+	private void example1() {
+		System.out.println("1");
+	}
+
+	private void example2() {
+		System.out.println("2");
+
+	}
+
+	private void example3() {
+		System.out.println("3");
+
+	}
+
+}
